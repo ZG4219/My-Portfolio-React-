535cb3f825e0bd454869f51d7ccc47d06319faa9
Dionne Angeles <jr8dionne@yahoo.com>
2023-02-27 15:31:08 -0800

    created css folder

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,28 @@
 import React from "react";
-import MyPage from "./components/MyPage";
+//import MyPage from "./components/MyPage";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <MyPage />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
