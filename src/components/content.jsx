328c105aa6a6c537654e79e7536bbd1200f1c831
Dionne Angeles <jr8dionne@yahoo.com>
2023-02-27 15:14:16 -0800

    created content.jsx

---
+++ src/components/content.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Content() {
+  return (
+    <section>
+      <h2>Content Section</h2>
+      <p>Dito ilagay ang iyong content!</p>
+    </section>
+  );
+}
+
+export default Content;
