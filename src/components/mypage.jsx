15177fe6c0c8def84a0d99f81fead58bbd76d476
Dionne Angeles <jr8dionne@yahoo.com>
2023-02-27 15:12:09 -0800

    added header.jsx

---
+++ src/components/mypage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
+
+function MyPage() {
+    return (
+      <div>
+        <Header />
+        <Content />
+        <Footer />
+      </div>
+    );
+  }
+  
+  export default MyPage;
+  
