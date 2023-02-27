27fcc8642b0a357b2669a20d19a723bf5bc616ef
Dionne Angeles <jr8dionne@yahoo.com>
2023-02-27 15:24:06 -0800

    input more components into header file

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 
 function Header() {
-  return (
-    <header>
-      <h1>My Portfolio</h1>
-    </header>
-  );
-}
-
-export default Header;
+    return (
+      <header>
+        <h1>Dionne Angeles</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+  
+  export default Header;
+  
