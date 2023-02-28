645b1205433f5a50a9cbc70a0a9565c86bca2329
Dionne Angeles <jr8dionne@yahoo.com>
2023-02-27 16:33:40 -0800

    modified fro jsx to .js

--- src/components/mypage.js
+++ src/components/mypage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./Header";
-import Content from "./Content";
-import Footer from "./Footer";
+import Content from "./content";
+import Footer from "./footer";
 
 function MyPage() {
     return (
