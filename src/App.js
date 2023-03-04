271a2ca6e3572007379ceea4fe4885a2df627611
Dionne Angeles <jr8dionne@yahoo.com>
2023-03-04 09:27:49 -0800

    added more components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-// import Home from './pages/Home';
-// import About from './pages/About';
-// import Contact from './pages/Contact';
 import './App.css';
 import MyPage from './components/mypage';
 
